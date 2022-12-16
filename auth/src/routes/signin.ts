1af728c99d5d516bed37d66cf9dd4c0ac52b0771
Sixtus Innocent <sixtusinno@gmail.com>
2022-12-16 14:14:03 +0100

    Signin and Signout auth flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,50 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
-router.get("/api/users/signin", (req, res) => {
-  res.send("Hello sign");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Must be a valid email!"),
+    body("password").trim().notEmpty().withMessage("Password is required!"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid Credential!");
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid Credential!");
+    }
+
+    const userToken = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userToken,
+    };
+
+    res.status(201).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
